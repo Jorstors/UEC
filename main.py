2106da8a3cb14c2abfd04a3b0d9d5ab9f8d98cbb
Justus Jones <justus1274@gmail.com>
2025-02-15 14:30:33 -0800

    Small Visual Tweak to Output

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "image_to_ascii"))
 from image_to_ascii.converter import image_to_ascii
 
-print(image_to_ascii("bookworm-151738_640.png", size=(50, 50), charset=' .:-=+*#%@'))
+print(image_to_ascii("./bookworm-151738_640.png", size=(50,50), charset=' .:-=+*#%@'[::-1]))
