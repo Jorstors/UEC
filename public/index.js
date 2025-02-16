22ad16b4a1f032ff4459a030bac97a72c37ef6cf
Justus Jones <justus1274@gmail.com>
2025-02-16 01:10:25 -0800

    Error message when kamiak is down

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ let userInput = "";
 let loadingID;
 
 inputBox.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
+  if (event.key === "Enter" && inputBox.value) {
     userInput = inputBox.value;
     // Clear the input and output boxes
     outputBox.innerText = "";
@@ -20,6 +20,7 @@ inputBox.addEventListener("keyup", function (event) {
 });
 
 inputButton.addEventListener("click", function () {
+  if (!inputBox.value) return;
   userInput = inputBox.value;
   // Clear the input and output boxes
   outputBox.innerText = "";
@@ -70,11 +71,31 @@ async function promptAPI() {
     "make a drawing of a high contrast, black and white, minimalistic ";
   const baseUrl = `/get-art?prompt=${appendedPrompts}${string}&size=`;
   for (var i = 2; i < 70; i += 1) {
-    const response = await fetch(baseUrl + i);
-    const data = await response.text();
-    // Update the output box with the ASCII art
-    clearSpinner();
-    outputBox.innerText = data;
+    try {
+      const response = await fetch(baseUrl + i);
+      if (!response.ok) throw new Error("Network response was not ok");
+      const data = await response.text();
+      // Update the output box with the ASCII art
+      clearSpinner();
+      outputBox.innerText = data;
+    } catch (error) {
+      // If an error occurs, display an error message and break the loop
+      clearSpinner();
+
+      // Load placeholder ASCII art in the output box
+      fetch("public/notavailable.txt")
+        .then((response) => response.text())
+        .then((data) => {
+          outputBox.innerHTML = data;
+        })
+        .catch((error) =>
+          console.error("Error loading error ASCII art:", error)
+        );
+
+      // Log the error
+      console.error("Error fetching data:", error);
+      break;
+    }
   }
 }
 
@@ -90,30 +111,6 @@ function debounce(func, timeout = 5 * 1000) {
 }
 
 const prompt = debounce(promptAPI, 3 * 1000);
-
-// @app.route("/get-art")
-// def getArt():
-//     filename = request.args.get("prompt") + ".png"
-//     if filename is None:
-//         return "Invalid request: prompt is required"
-//     if not os.path.isfile(filename):
-//         generate_images(request.args["prompt"])
-
-//     s = request.args.get("size", 1, type=int)
-//     asc = image_to_ascii(filename, size=(s,s), charset=' .:-=+*#%@')
-
-//     return asc
-
-// Load placeholder ASCII art in the output box
-
-fetch("public/wormy.txt")
-  .then((response) => response.text())
-  .then((data) => {
-    outputBox.innerHTML = data;
-  })
-  .catch((error) =>
-    console.error("Error loading placeholder ASCII art:", error)
-  );
 
 // @app.route("/get-art")
 // def getArt():
