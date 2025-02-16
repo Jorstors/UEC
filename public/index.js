d46c699c0080447ec3c24c2292ccd105fff46313
Justus Jones <justus1274@gmail.com>
2025-02-16 00:01:48 -0800

    debounced search function

--- public/index.js
+++ public/index.js
@@ -11,7 +11,6 @@ inputBox.addEventListener("keyup", function (event) {
     // Clear the input and output boxes
     outputBox.innerText = "";
     inputBox.value = "";
-    console.log(userInput);
     // If there is a spinner running, clear it
     if (loadingID) clearSpinner();
     loadingID = loading();
@@ -25,7 +24,6 @@ inputButton.addEventListener("click", function () {
   // Clear the input and output boxes
   outputBox.innerText = "";
   inputBox.value = "";
-  console.log(userInput);
   // If there is a spinner running, clear it
   if (loadingID) clearSpinner();
   loadingID = loading();
@@ -37,7 +35,7 @@ copyButton.addEventListener("click", function () {
   // Copy the text from the output div
   let text = outputBox.innerText;
   navigator.clipboard.writeText(text);
-  console.log("Copied to clipboard");
+  console.log("Copied to clipboard!");
 });
 
 // ASCII LOADER
@@ -66,21 +64,56 @@ function clearSpinner() {
 }
 
 // API call to backend through HTTPS request
-async function prompt() {
+async function promptAPI() {
   const string = userInput;
   const appendedPrompts =
     "make a drawing of a high contrast, black and white, minimalistic ";
-  const baseUrl = `/get-art?prompt=${appendedPrompts}${string}&size=`
+  const baseUrl = `/get-art?prompt=${appendedPrompts}${string}&size=`;
   for (var i = 2; i < 70; i += 1) {
-      const response = await fetch(
-        baseUrl + i
-      );
-      const data = await response.text();
-      // Update the output box with the ASCII art
-      clearSpinner();
-      outputBox.innerText = data;
+    const response = await fetch(baseUrl + i);
+    const data = await response.text();
+    // Update the output box with the ASCII art
+    clearSpinner();
+    outputBox.innerText = data;
   }
 }
+
+// Debounced version of the prompt function
+function debounce(func, timeout = 5 * 1000) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
+}
+
+const prompt = debounce(promptAPI, 3 * 1000);
+
+// @app.route("/get-art")
+// def getArt():
+//     filename = request.args.get("prompt") + ".png"
+//     if filename is None:
+//         return "Invalid request: prompt is required"
+//     if not os.path.isfile(filename):
+//         generate_images(request.args["prompt"])
+
+//     s = request.args.get("size", 1, type=int)
+//     asc = image_to_ascii(filename, size=(s,s), charset=' .:-=+*#%@')
+
+//     return asc
+
+// Load placeholder ASCII art in the output box
+
+fetch("public/wormy.txt")
+  .then((response) => response.text())
+  .then((data) => {
+    outputBox.innerHTML = data;
+  })
+  .catch((error) =>
+    console.error("Error loading placeholder ASCII art:", error)
+  );
 
 // @app.route("/get-art")
 // def getArt():
